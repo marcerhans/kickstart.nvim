a42e0cdaa2ae8e48672ad411e95b3f62b3eca1b8
marcerhans <marcerhans@gmail.com>
2025-10-05 20:31:42 +0200

    ...

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = 'ö'
+vim.g.maplocalleader = 'ö'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -1015,5 +1015,5 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.keymap.set('n', 'öb', '<CMD>bp<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'ön', '<CMD>bn<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'äb', '<CMD>bp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'än', '<CMD>bn<CR>', { noremap = true, silent = true })
