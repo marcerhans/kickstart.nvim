4198745c33a9550f2b7978f212a0db34be37955c
marcerhans <marcerhans@gmail.com>
2025-10-05 20:29:02 +0200

    ...

--- init.lua
+++ init.lua
@@ -1014,3 +1014,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.keymap.set('n', 'öb', '<CMD>bp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'ön', '<CMD>bn<CR>', { noremap = true, silent = true })
